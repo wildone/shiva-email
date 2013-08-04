2c802df6d44e6c4e22c8e61acddb8dab81d3e8a8
tzrlk <peterc@aetheric.co.nz>
2013-08-04 19:02:15 +1200

    [GH-4] Defined the interfaces for mail content storage/retrieval.

---
+++ src/main/java/nz/co/aetheric/shiva/email/MailContent.java
@@ -0,0 +1,36 @@
+package nz.co.aetheric.shiva.email;
+
+import java.util.Locale;
+
+/**
+ * This interface defines the data expected to be present in a content record. Records should be unique on
+ * {@link #getKey}, {@link #getLocale}, and {@link #getType}.
+ * <p>Author: <a href="http://gplus.to/tzrlk">Peter Cummuskey</a></p>
+ */
+public interface MailContent {
+
+	/**
+	 * The key that is used to store and retrieve the email content.
+	 * @return The key under which the content is stored.
+	 */
+	public String getKey();
+
+	/**
+	 * The locale targeting for the email content.
+	 * @return The locale for which the content is targeted.
+	 */
+	public Locale getLocale();
+
+	/**
+	 * Whether the content is plain, html, markdown, velocity, etc.
+	 * @return The type of rendering required for the content.
+	 */
+	public String getType();
+
+	/**
+	 * The content stored in this record.
+	 * @return The content itself.
+	 */
+	public String getBody();
+
+}
---
+++ src/main/java/nz/co/aetheric/shiva/email/MailContentProvider.java
@@ -0,0 +1,31 @@
+package nz.co.aetheric.shiva.email;
+
+import java.util.Collection;
+import java.util.Locale;
+
+/**
+ * Provides a service definition for implementations that retrieve email content from some kind of persistent storage.
+ * <p>Author: <a href="http://gplus.to/tzrlk">Peter Cummuskey</a></p>
+ */
+public interface MailContentProvider {
+
+	/**
+	 * Retrieve the mail content stored under the provided key, locale and rendering type.
+	 * @param key The under which the content is stored under.
+	 * @param locale The locale for which the content is targeted.
+	 * @param type The rendering type of the content.
+	 * @return An instance of the mail content data.
+	 * @throws ShivaMailException
+	 */
+	public MailContent getMailContent(String key, Locale locale, String type) throws ShivaMailException;
+
+	/**
+	 * Retrieves a collection of all the content stored under the provided key and locale.
+	 * @param key The key under which the content is stored.
+	 * @param locale The locale for which the content is targeted.
+	 * @return All matching instances of the mail content data.
+	 * @throws ShivaMailException
+	 */
+	public Collection<MailContent> getMailContent(String key, Locale locale) throws ShivaMailException;
+
+}
