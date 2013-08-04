4da38e76a45ab111a296d1b753408c61748bbbe3
tzrlk <peterc@aetheric.co.nz>
2013-08-04 19:46:56 +1200

    [GH-4] Simplified the main ShivaMail service interface more.

--- src/main/java/nz/co/aetheric/shiva/email/ShivaMail.java
+++ src/main/java/nz/co/aetheric/shiva/email/ShivaMail.java
@@ -2,6 +2,7 @@ package nz.co.aetheric.shiva.email;
 
 import java.util.Collection;
 import java.util.Locale;
+import java.util.Map;
 
 /**
  * This service provides a no-hassle way of sending pre-configured email to recipients.
@@ -9,10 +10,15 @@ import java.util.Locale;
  */
 public interface ShivaMail {
 
-    void sendMail(ShivaMailPacket packet) throws ShivaMailException;
-
-    void sendMail(String code, Collection<String> recipients) throws ShivaMailException;
-
-    void sendMail(String code, Locale locale, Collection<String> recipients) throws ShivaMailException;
+	/**
+	 * This method will retrieve, render and send a specified email to the provided recipients, throwing an exception
+	 * if anything goes wrong during the process.
+	 * @param code The code of the email content.
+	 * @param locale The locale of the email content.
+	 * @param data The data to use while rendering the email.
+	 * @param recipients The recipients to send the email to.
+	 * @throws ShivaMailException
+	 */
+	public void send(String code, Locale locale, Map data, Collection<Recipient> recipients) throws ShivaMailException;
 
 }
