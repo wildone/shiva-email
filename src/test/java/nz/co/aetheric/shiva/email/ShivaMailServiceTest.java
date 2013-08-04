28d9a15f9fcfbf3ca8e517bbd6fbbf6b15f61126
tzrlk <peterc@aetheric.co.nz>
2013-08-04 18:32:21 +1200

    [GH-4] Removed all the obsolete tests.

--- src/test/java/nz/co/aetheric/shiva/email/ShivaMailServiceTest.java
+++
@@ -1,8 +0,0 @@
-package nz.co.aetheric.shiva.email;
-
-/**
- * Unit testing for {@link ShivaMailService} implementation of {@link ShivaMail}.
- * <p>Author: <a href="http://gplus.to/tzrlk">Peter Cummuskey</a></p>
- */
-public class ShivaMailServiceTest {
-}
