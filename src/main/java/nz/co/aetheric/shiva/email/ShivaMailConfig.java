b651d55e1367669e4344a8b2364cd46a5b0ebb30
tzrlk <peterc@aetheric.co.nz>
2013-08-04 18:33:19 +1200

    [GH-4] Turned ShivaMailConfig into an interface.

--- src/main/java/nz/co/aetheric/shiva/email/ShivaMailConfig.java
+++ src/main/java/nz/co/aetheric/shiva/email/ShivaMailConfig.java
@@ -1,27 +1,15 @@
 package nz.co.aetheric.shiva.email;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-
 /**
  * This class aggregates all the configuration information needed for running Shiva mail.
  * <p>Author: <a href="http://gplus.to/tzrlk">Peter Cummuskey</a></p>
  */
-@Service
-public class ShivaMailConfig {
+public interface ShivaMailConfig {
 
-    @Value("${shiva.mail.sender}")
-    String mailSender;
+    public String getDefaultSender();
 
-    @Value("${shiva.mail.copyto:}")
-    String copyRecipient;
+    public String getDefaultCopyRecipient();
 
-    public String getMailSender() {
-        return mailSender;
-    }
-
-    public String getCopyRecipient() {
-        return copyRecipient;
-    }
+	public String getDefaultSubject();
 
 }
